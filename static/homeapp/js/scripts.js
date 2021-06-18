0931f93ffe9f84526d41248300a9362ba8fd9a8c
unknown <amanayankhan78@gmail.com>
2021-06-18 09:02:38 +0530

    bucket completed

--- static/homeapp/js/scripts.js
+++ static/homeapp/js/scripts.js
@@ -5,15 +5,21 @@ function getuserinfo(obj) {
   let info = document.getElementById("myinfo");
   let login = document.getElementById("last-login");
   let source = obj.firstElementChild.firstElementChild.getAttribute("src");
+  let anchor = document.getElementById('anchor')
+  let a = document.createElement('a')
+  a.href = "/logsys/uprofile/"+obj.id
+  a.text="Check out my profile"
   let img = document.createElement("img");
   img.src = source.replace("35", "200");
   img.setAttribute("class", "img-fluid");
 
   axios.get("/logsys/user/" + obj.id).then((response) => {
+    anchor.innerHTML =""
+    anchor.appendChild(a)
     uname.innerHTML =
       response.data.username.charAt(0).toUpperCase() +
       response.data.username.slice(1).toLowerCase();
-    login.innerHTML = response.data.last_login.split("T")[0];
+    login.innerHTML = "Last Seen: " + response.data.last_login.split("T")[0];
     info.innerHTML = "";
     info.appendChild(img);
   });
