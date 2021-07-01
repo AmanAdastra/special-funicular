66e4fef3282d79d5b0ab3d30582a8b549aa03bfb
unknown <amanayankhan78@gmail.com>
2021-07-01 11:06:19 +0530

    circular button added

--- homeapp/admin.py
+++ homeapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Bucket,Article
+from .models import Bucket,Article,Project
 # Register your models here.
 
 @admin.register(Bucket)
 class AdminBucket(admin.ModelAdmin):
+    list_display = ['id','image','notes','date','user']
+
+@admin.register(Project)
+class AdminProject(admin.ModelAdmin):
     list_display = ['id','image','notes','date','user']
     
     
